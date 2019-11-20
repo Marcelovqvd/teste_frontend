d48af6143d6cba48c8838b66d20f0381dd04bea5
marcelovqvd <marcelovqvd@gmail.com>
2019-11-19 22:58:40 -0300

    styled components e estilo global

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Routes from './routes';
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Main</h1>
-    </div>
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
   );
 }
 
