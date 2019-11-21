a8546bad4009c59b81f2f6aad009d192e8e029eb
marcelovqvd <marcelovqvd@gmail.com>
2019-11-20 23:21:24 -0300

    axios e baseUrl

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Title } from './styles';
 function Main() {
 
-  return <h1>Main</h1>
+  return <h1>SignUp</h1>
 }
 
 export default Main;
---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseUrl: 'https://reqres.in/api';
+});
+
+export default api;
