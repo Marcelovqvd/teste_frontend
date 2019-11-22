2ea5ed809947420bf9ddc17b262d693ec2e70c21
marcelovqvd <marcelovqvd@gmail.com>
2019-11-21 21:44:38 -0300

    Componente Header

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 
+import Header from './components/Header';
+
 function App() {
   return (
     <>
+      <Header />
       <Routes />
       <GlobalStyle />
     </>
---
+++ src/components/Header/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h1 {
+    font-size: 24px;
+  color: #fff;
+  }
+`;
